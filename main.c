30c278379eb38e78edc2f03cc00a9ad93884111a
Maury <gentilemaurizio6@gmail.com>
2026-09-12 12:55:54 +0200

    test 6

--- main.c
+++ main.c
@@ -7,10 +7,11 @@
 #include <windows.h>
 #include <stdio.h>
 #include <shellapi.h>
+#include <string.h>
 #include <urlmon.h>
 
 // --- CONFIGURATION & CONSTANTS ---
-#define CURRENT_VERSION 5
+#define CURRENT_VERSION 6
 #define GITHUB_VERSION_URL "https://raw.githubusercontent.com/MauryDevIta/NanoBattery/refs/heads/main/version.txt"
 #define GITHUB_EXE_URL     "https://raw.githubusercontent.com/MauryDevIta/NanoBattery/refs/heads/main/battery_widget.exe"
 
@@ -175,7 +176,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
             SelectObject(hdc, hFont);
             SetBkMode(hdc, TRANSPARENT);
-            SetTextColor(hdc, RGB(0, 255, 255));
+            SetTextColor(hdc, RGB(255, 255, 255));
 
             RECT rect;
             GetClientRect(hwnd, &rect);
